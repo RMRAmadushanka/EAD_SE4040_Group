11f99246e3a326b3d0ba9b229f26be225fb6c4ce
chalakanim <it20106638@my.sliit.lk>
2023-10-08 23:35:13 +0530

    added user collection

---
+++ web_server/Collections/User.cs
@@ -0,0 +1,44 @@
+using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+
+ 
+
+namespace web.server.Collections
+{
+    public class User
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+ 
+
+        [BsonElement("Username")]
+        public string Username { get; set; }
+
+ 
+
+        [BsonElement("Password")]
+        public string Password { get; set; }
+
+ 
+
+        [BsonElement("Role")]
+        public string Role { get; set; }
+
+ 
+
+        [BsonElement("NIC")]
+        public string NIC { get; set; }
+
+ 
+
+        [BsonElement("TicketBookingIds")]
+        public List<string> TicketBookingIds { get; set; }
+
+ 
+
+        [BsonElement("IsActive")]
+        public bool IsActive { get; set; }
+    }
+}
