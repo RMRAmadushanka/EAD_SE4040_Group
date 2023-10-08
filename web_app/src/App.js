77adbceb97d2179fd1a4c4fff06b50638514b30d
RMRAmadushanka <it19147260@my.sliit.lk>
2023-10-08 22:49:00 +0530

    Add Routes

--- web_app/src/App.js
+++ web_app/src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from 'react';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import PrivateRoute from './components/common/PrivateRoute';
+
+import Login from './components/auth/Login';
+
+import Profile from './components/profile/Profile';
+
+import { Register } from './components/auth/Register';
+import Home from './components/home/Home';
+import { Backoffice } from './components/Pages/Backoffice';
+import Travelagent from './components/Pages/Travelagent';
+import TravelAccountManagement from './components/Pages/TravelAccountManagement';
+import TrainScheduleManagement from './components/Pages/TrainScheduleManagement';
 
 function App() {
+    
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+          <Route path="/lk" element={<Login />}/>
+          <Route path="/register" element={<Register />}/>  
+          <Route exact path="/profile" element={<Profile />} />
+          <Route exact path="/backoffice" element={<Backoffice />} />
+          <Route exact path="/travelagent" element={<Profile />} />
+          <Route exact path="/sds" element={<TravelAccountManagement />} />
+          <Route exact path="/" element={<TrainScheduleManagement />} />
+      </Routes>
+         
+    </Router>
   );
 }
 
