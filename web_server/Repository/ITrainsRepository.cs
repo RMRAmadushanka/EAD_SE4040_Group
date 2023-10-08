0287cd52df3f36f39744ada4135e521635e7e6c9
it20228644 <it20228644@my.sliit.lk>
2023-10-08 23:35:32 +0530

    Train repository updated

--- web_server/Repository/ITrainsRepository.cs
+++ web_server/Repository/ITrainsRepository.cs
@@ -1,8 +1,8 @@
-using WEB_SERVER.Collections;
+using web_server.Collections;
 
  
 
-namespace WEB_SERVER.Repository
+namespace web_server.Repository
 {
     public interface ITrainsRepository
     {
