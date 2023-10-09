c52ea8e052f3d879eee6b92872978d6cc0bf7046
chathura1999 <it20014568@my.sliit.lk>
2023-10-09 23:02:07 +0530

    Iticket booking

--- web_server/Repository/ITicketBookingRepository.cs
+++ web_server/Repository/ITicketBookingRepository.cs
@@ -1,6 +1,13 @@
-﻿namespace web_server.Repository
+﻿using web_server.Collections;
+
+namespace web_server.Repository
 {
     public interface ITicketBookingRepository
     {
+        Task CreateReservation(string userId, string trainId, TicketBooking newBooking);
+        List<TicketBooking> GetBookingsByUserId(string userId);
+        Task<bool> UpdateReservationDateAsync(string id, string newReservationDate);
+        TicketBooking GetReservationById(string reservationId);
+        void CancelReservation(string reservationId);
     }
 }
